329e0e62235d3f349c27eab9e2563749ac44bf88
Caronte9029 <80341092+Caronte9029@users.noreply.github.com>
2023-04-11 14:15:46 -0600

    Add files via upload

---
+++ Vista/Administrador/Administrador_View.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="http://localhost/Biblioteca%20digital/Recursos/img/nm02.ico" >
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administración</title>
+    <link rel="stylesheet" href="http://localhost/Biblioteca%20digital/Recursos/css/Administrador.css">
+   
+</head>
+<body>
+<div class="container">
+    <aside>
+        <div class="iconosNav">
+            <div class="contentIcon">
+                <img src="http://localhost/Biblioteca%20digital/Recursos/img/nm02.png">
+            </div>
+            <div class="contentIcon">
+                <h2>Insertar</h2>
+            </div>
+            <div class="contentIcon">
+                <h2>Modificar</h2>
+            </div>
+            <div class="contentIcon">
+                <h2>Borrar</h2>
+            </div>
+            <div class="contentIcon">
+                <h2>Agregar Usuario</h2>
+            </div>
+        </div>
+    </aside>
+</div>
+
+
+<script src="http://localhost/Biblioteca%20digital/Recursos/js/menuLateral.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header('Location: ./Vista/login/login_View.php');
+require_once("./Vista/login/login_View.php");
+?>
